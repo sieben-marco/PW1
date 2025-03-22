3cb6f6dbe86ff60255c1fbce404af0a955d2355c
Marco Sieben <sociais.marco@gmail.com>
2025-03-21 21:30:57 -0300

    arquivos da aula sobre Collection

---
+++ Aulas/AulaColecoes/src/Conjuntos.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class Conjuntos {
+	public static void main(String[] args) {
+		// criar um conjunto usando a tabela hash
+		HashSet<String> conjunto = new HashSet<>();
+		
+		// add
+		conjunto.add("carlos");
+		conjunto.add("eduardo");
+		conjunto.add("josé");
+		conjunto.add("marcos");
+		conjunto.add("josé");
+		
+		// listar todo o conjunto
+		for (String c : conjunto) {
+			System.out.println(c);
+		}
+	}
+}
---
+++ Aulas/AulaColecoes/src/Cpf.java
@@ -0,0 +1,37 @@
+
+public class Cpf implements Comparable<Cpf> {
+	private long numero;
+	private int digito;
+	
+	public int compareTo(Cpf cpf) {
+		if (digito > cpf.getNumero()) return 1;
+		if (digito < cpf.getNumero()) return -1;
+		return 0;
+	}
+	
+	public Cpf(long numero, int digito) {
+		this.digito = digito;
+		this.numero = numero;
+	}
+	
+	public int getDigito() {
+		return this.digito;
+	}
+	
+	public void setDigito(int digito) {
+		this.digito = digito;
+	}
+	
+	public long getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cpf [digito=" + this.digito + ", numero=" + this.numero + "]";
+	}
+}
